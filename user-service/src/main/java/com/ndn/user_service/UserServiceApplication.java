b2b3887ac8ab19291d93a241bbd54d1a46fcaa1d
NandanVasistaBH <nandan.vasista.bh@gmail.com>
2024-10-01 08:58:23 +0530

    init the project setup and wrote user api

--- user-service/src/main/java/com/ndn/user_service/UserServiceApplication.java
+++ user-service/src/main/java/com/ndn/user_service/UserServiceApplication.java
@@ -9,7 +9,7 @@ public class UserServiceApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(UserServiceApplication.class, args);
-		// System.out.println("user service is running");
+		System.out.println("user service is running");
 	}
 
 }
