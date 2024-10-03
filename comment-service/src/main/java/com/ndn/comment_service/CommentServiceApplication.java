44b471d1ffcb7e26601f9b9c2097ec70151f8a72
NandanVasistaBH <nandan.vasista.bh@gmail.com>
2024-10-03 09:26:17 +0530

    configuring comment service

--- comment-service/src/main/java/com/ndn/comment_service/CommentServiceApplication.java
+++ comment-service/src/main/java/com/ndn/comment_service/CommentServiceApplication.java
@@ -2,12 +2,22 @@ package com.ndn.comment_service;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.client.loadbalancer.LoadBalanced;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.client.RestTemplate;
 
+@EnableDiscoveryClient
 @SpringBootApplication
 public class CommentServiceApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(CommentServiceApplication.class, args);
+	}
+	@LoadBalanced
+	@Bean
+	public RestTemplate getRestTemplate(){
+		return new RestTemplate();
 	}
 
 }
